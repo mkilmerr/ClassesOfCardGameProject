c11940b9b9bf941ba24d12d5b54c5fb4fc89c19f
mkilmer <marcoskilmer1@gmail.com>
2020-06-02 17:51:45 -0300

    Initial Commit

---
+++ ClassesOfCardGameProject/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  ClassesOfCardGameProject
+//
+//  Created by Marcos Kilmer on 02/06/20.
+//  Copyright © 2020 mkilmer. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // insert code here...
+        NSLog(@"Hello, World!");
+    }
+    return 0;
+}
